d6d5f701cb1a3d6503d300d34b1952c794af760e
group kabear <adibahm0@gmail.com>
2026-05-15 21:38:02 +0300

    Remove success message on user account insertion
    Remove success message after user account creation.

--- insert_user.php
+++ insert_user.php
@@ -8,7 +8,6 @@ $password = $_POST['password'];
 $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
 
 if (mysqli_query($conn, $sql)) {
-    echo "تم إنشاء الحساب بنجاح";
     header("Location: login.html");
     exit();
 } else {
